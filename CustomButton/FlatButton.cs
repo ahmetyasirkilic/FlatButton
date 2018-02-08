fdaf8d4c379b506bf82d984f07d8f37c0bd49d79
Ahmet Yasir Kılıç <manilaorleans@gmail.com>
2018-02-08 15:06:59 +0300

    second commit

---
+++ CustomButton/FlatButton.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomButton
+{
+    class FlatButton : Control
+    {
+        private SolidBrush borderBrush, textBrush;
+        private Rectangle borderRectangle;
+        private bool active = false;
+        private StringFormat stringFormat = new StringFormat();
+
+        public override Cursor Cursor { get; set; } = Cursors.Hand;
+        public float BorderThickness { get; set; } = 2;
+
+        public FlatButton()
+        {
+            borderBrush = new SolidBrush(ColorTranslator.FromHtml("#31302b"));
+            textBrush = new SolidBrush(ColorTranslator.FromHtml("#FFF"));
+
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+
+            this.Paint += FlatButton_Paint;
+        }
+
+        private void FlatButton_Paint(object sender, PaintEventArgs e)
+        {
+            borderRectangle = new Rectangle(0, 0, Width, Height);
+            e.Graphics.DrawRectangle(new Pen(borderBrush, BorderThickness), borderRectangle);
+            e.Graphics.DrawString(this.Text, this.Font, (active) ? textBrush : borderBrush, borderRectangle, stringFormat);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            base.BackColor = ColorTranslator.FromHtml("#31302b");
+            active = true;
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            base.BackColor = ColorTranslator.FromHtml("#FFF");
+            active = false;
+        }
+    }
+}
---
+++ CustomButton/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CustomButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flatButton1 = new CustomButton.FlatButton();
+            this.SuspendLayout();
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BorderThickness = 2F;
+            this.flatButton1.Location = new System.Drawing.Point(67, 103);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Size = new System.Drawing.Size(150, 25);
+            this.flatButton1.TabIndex = 0;
+            this.flatButton1.Text = "BUTTON";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.flatButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatButton flatButton1;
+    }
+}
+
